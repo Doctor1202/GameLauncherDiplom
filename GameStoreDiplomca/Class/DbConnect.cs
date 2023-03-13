cce588cc3543afca6f741ff9a1e7ab5c81134b02
Жора <zdrozda1202@gmail.com>
2023-03-13 18:18:28 +0200

    Add DbConnect and connect to the local host

---
+++ GameStoreDiplomca/Class/DbConnect.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GameStoreDiplomca.Class
+{
+    public class DbConnect
+    {
+        public static MongoClient dbClient { get; private set; }
+
+        public static void ConnectionToDb()
+        {
+            try
+            {
+                dbClient = new MongoClient("mongodb://localhost:27017");
+            }
+            catch(Exception ex) 
+            {
+                MessageBox.Show("Error" + ex);
+                Environment.Exit(1);
+            }
+        } 
+    }
+}
