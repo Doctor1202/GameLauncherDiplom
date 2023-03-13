d0a96ea53f6a21c911c7ab45b77d07c0ec2f3840
Жора <zdrozda1202@gmail.com>
2023-03-13 19:22:35 +0200

    Add User.cs in to the folder Class

---
+++ GameStoreDiplomca/Class/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStoreDiplomca.Class
+{
+    public class User
+    {
+        public string UserName { get; private set; }
+        public string Login { get; private set; }
+        public string Password { get; private set; }
+        public decimal Money { get; private set; }
+        public bool IsAdmin { get; private set; }
+
+
+    }
+}
