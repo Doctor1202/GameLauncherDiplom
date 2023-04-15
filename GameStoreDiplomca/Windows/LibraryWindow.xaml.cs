0066bb7198a49f4677a5aa576f0f21295868789a
Жора <zdrozda1202@gmail.com>
2023-04-15 21:37:50 +0300

    LibraryWindow is ready

--- GameStoreDiplomca/Windows/LibraryWindow.xaml.cs
+++ GameStoreDiplomca/Windows/LibraryWindow.xaml.cs
@@ -63,16 +63,48 @@ namespace GameStoreDiplomca.Windows
 
         private void Play_Button_Click(object sender, RoutedEventArgs e)
         {
-
+            MessageBox.Show("Your game was started");
         }
 
         private void Download_Button_Click(object sender, RoutedEventArgs e)
         {
+            ULibrary lg = (ULibrary)GameList_DataGid.SelectedItem;
+            var userName = UserName_Text.Text;
 
+            var storeDB = DbConnect.dbClient.GetDatabase("StoreDB");
+            var collection = storeDB.GetCollection<User>("User");
+            var filter = Builders<User>.Filter.Eq("UserName", userName) &
+                         Builders<User>.Filter.Eq("ULibrary.GameName", lg.GameName);
+            var game = collection.Find(filter).FirstOrDefault();
+
+            var update = Builders<User>.Update.Set("ULibrary.$.IsDownLoad", true);
+
+            collection.UpdateOne(filter, update);
+
+            MessageBox.Show("Game was delete from your computer.");
+        }
+
+        private void Delete_Button_Click(object sender, RoutedEventArgs e)
+        {
+            ULibrary lg = (ULibrary)GameList_DataGid.SelectedItem;
+            var userName = UserName_Text.Text;
+
+            var storeDB = DbConnect.dbClient.GetDatabase("StoreDB");
+            var collection = storeDB.GetCollection<User>("User");
+            var filter = Builders<User>.Filter.Eq("UserName", userName) &  
+                         Builders<User>.Filter.Eq("ULibrary.GameName", lg.GameName);
+            var game = collection.Find(filter).FirstOrDefault();
+
+            var update = Builders<User>.Update.Set("ULibrary.$.IsDownLoad", false);
+
+            collection.UpdateOne(filter, update);
+
+            MessageBox.Show("Game was delete from your computer.");
         }
 
         private void GameList_DataGid_MouseUp(object sender, MouseButtonEventArgs e)
         {
+
             ULibrary lg = (ULibrary)GameList_DataGid.SelectedItem;
 
             GameName_TextBox.Text = lg.GameName;
@@ -81,14 +113,19 @@ namespace GameStoreDiplomca.Windows
             {
                 Download_Button.IsEnabled = true;
                 Play_Button.IsEnabled = false;
+                Delete_Button.IsEnabled = false;
             }
             else
             {
                 Download_Button.IsEnabled = false;
                 Play_Button.IsEnabled = true;
+                Delete_Button.IsEnabled = true;
             }
+        }
 
-
+        private void LibraryWindow1_Loaded(object sender, RoutedEventArgs e)
+        {
+            LReadAllDocument();
         }
     }
 }
