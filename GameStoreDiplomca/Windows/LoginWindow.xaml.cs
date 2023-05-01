a1a17909e0c6229959377067cbfc4d54cc8c225f
Жора <zdrozda1202@gmail.com>
2023-05-01 12:05:03 +0300

    refactoring login

--- GameStoreDiplomca/Windows/LoginWindow.xaml.cs
+++ GameStoreDiplomca/Windows/LoginWindow.xaml.cs
@@ -3,7 +3,6 @@ using MongoDB.Driver;
 using System;
 using System.Linq;
 using System.Windows;
-using BCrypt.Net;
 
 namespace GameStoreDiplomca.Windows
 {
@@ -18,48 +17,53 @@ namespace GameStoreDiplomca.Windows
             DbConnect.ConnectionToDb();
         }
 
+        public void mainLogin()
+        {
+            MainWindow main = new MainWindow();
+
+            string logInBox = LogIn_Box.Text;
+            string passWordBox = PassWord_Box.Password;
+
+            var storeDB = DbConnect.dbClient.GetDatabase("StoreDB");
+            var collection = storeDB.GetCollection<User>("User");
+            var searchFilter = Builders<User>.Filter.Eq("Login", logInBox);
+            var logInUser = collection.Find(searchFilter).FirstOrDefault();
+
+
+            if (logInUser is null)
+            {
+                MessageBox.Show("-");
+            }
+            else
+            {
+                var comparePass = BCrypt.Net.BCrypt.Verify(passWordBox, logInUser.Password);
+
+                if (!comparePass)
+                {
+                    MessageBox.Show("-");
+                }
+                else
+                {
+                    MessageBox.Show("+");
+                    main.Show();
+                    Close();
+
+                    main.User_Text.Text = logInUser.UserName;
+                    main.MoneyCoint_Text.Text = logInUser.Money.ToString();
+
+                    main.Change_Button.Visibility = logInUser.IsAdmin ? Visibility.Visible : Visibility.Hidden;
+                    main.Create_Button.Visibility = logInUser.IsAdmin ? Visibility.Visible : Visibility.Hidden;
+                    main.Delete_Button.Visibility = logInUser.IsAdmin ? Visibility.Visible : Visibility.Hidden;
+                }
+            }
+
+        }
+
         private void Login_Button_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                MainWindow main = new MainWindow();
-
-                string logInBox = LogIn_Box.Text;
-                string passWordBox = PassWord_Box.Password;
-
-                var storeDB = DbConnect.dbClient.GetDatabase("StoreDB");
-                var collection = storeDB.GetCollection<User>("User");
-                var searchFilter = Builders<User>.Filter.Eq("Login", logInBox);
-                var logInUser = collection.Find(searchFilter).FirstOrDefault();
-
-
-                if (logInUser is not null)
-                {
-                    var comparePass = BCrypt.Net.BCrypt.Verify(passWordBox, logInUser.Password);
-
-                    if (comparePass)
-                    {
-                        MessageBox.Show("+");
-                        main.Show();
-                        Close();
-
-                        main.User_Text.Text = logInUser.UserName;
-                        main.MoneyCoint_Text.Text = logInUser.Money.ToString();
-
-                            main.Change_Button.Visibility = logInUser.IsAdmin ? Visibility.Visible : Visibility.Hidden;
-                            main.Create_Button.Visibility = logInUser.IsAdmin ? Visibility.Visible : Visibility.Hidden;
-                            main.Delete_Button.Visibility = logInUser.IsAdmin ? Visibility.Visible : Visibility.Hidden;
-
-                    }
-                    else
-                    {
-                        MessageBox.Show("-");
-                    }
-                }
-                else
-                {
-                    MessageBox.Show("-");
-                }
+                mainLogin();
             }
             catch (Exception ex) { MessageBox.Show("Error" + ex); }
         }
